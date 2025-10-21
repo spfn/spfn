15206ca97bdaf7a4b451597c8d8d2aa918c0278a
rayim <tonite32@gmail.com>
2025-10-21 22:43:08 +0900

    업데이트된 차트 반영

--- apps/landing/next.config.ts
+++ apps/landing/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   webpack(config) {
     // SVGR: Import SVG as React components
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
